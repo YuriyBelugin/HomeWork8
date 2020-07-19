c9c362d09135054c0e36eb606300b36c9ed2e346
Yury Bialuhin <yurka_belugin@mail.ru>
2020-07-19 23:24:30 +0300

    main class

---
+++ src/com/company/Main.java
@@ -0,0 +1,61 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        Map<Integer,Integer> letters=new HashMap<>();
+
+        ArrayList<Integer> nums = new ArrayList<>();
+            nums.add(random.getRandom());
+            nums.add(random.getRandom());
+            nums.add(random.getRandom());
+            nums.add(random.getRandom());
+            nums.add(random.getRandom());
+            nums.add(random.getRandom());
+            nums.add(random.getRandom());
+            nums.add(random.getRandom());
+            nums.add(random.getRandom());
+            nums.add(random.getRandom());
+            nums.add(random.getRandom());
+            nums.add(random.getRandom());
+            nums.add(random.getRandom());
+            nums.add(random.getRandom());
+            nums.add(random.getRandom());
+            nums.add(random.getRandom());
+            nums.add(random.getRandom());
+            nums.add(random.getRandom());
+            nums.add(random.getRandom());
+            nums.add(random.getRandom());
+            nums.add(random.getRandom());
+
+        System.out.println(nums);
+
+        for (int i = 0; i < nums.size(); i++) {
+            Integer tempInt = nums.get(i);
+            if (!letters.containsKey(tempInt)) {
+                letters.put(tempInt,1);
+            }
+            else if(letters.containsValue(3)) {
+                break;
+            }
+            else if(letters.containsValue(1)){
+                letters.put(tempInt, letters.get(tempInt)+1);
+            }
+
+            System.out.println(nums.get(i));
+
+
+        }
+    }
+}
+
+
+
+
+
+
---
+++ src/com/company/Random.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Random {
+    int a=10;
+    int b=1;
+
+    public int getRandom(){
+        return (int)(Math.random()*a)+b;
+    }
+
+}
